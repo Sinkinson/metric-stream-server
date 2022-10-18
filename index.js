85b6a32fa5e28d3c7202e1f59fb9015c30bd0e64
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2022-10-18 12:43:47 +0200

    port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4318;
+const PORT = process.env.PORT || 3002;
 const { parseRecord } = require('./utils/parser');
 
 app.use(express.json());
